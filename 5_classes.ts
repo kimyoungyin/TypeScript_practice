1fedb749ef55e7c84d589b4234a4632d86b16c8e
kimyoungyin <yin199859@gmail.com>
2021-10-29 23:10:54 +0900

    feat:  상속(inheritance)

--- 5_classes.ts
+++ 5_classes.ts
@@ -18,16 +18,38 @@ class Department {
     }
 }
 
-const accounting = new Department("d1", "Accounting");
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        // name 프로퍼티는 인스턴스 생성 시 name을 입력받지 않고 "IT"로 결정되므로 이렇게 작성
+        super(id, "IT");
+    }
+}
 
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-accounting.describe();
+class AccountingDepartment extends Department {
+    constructor(id: string, public reports: string[]) {
+        // name 프로퍼티는 인스턴스 생성 시 name을 입력받지 않고 "IT"로 결정되므로 이렇게 작성
+        super(id, "Accounting");
+    }
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+it.describe();
 // accounting.employees;
 // accounting.employees[2] = "Halo";
 
-accounting.printEmployeeInformation();
+it.printEmployeeInformation();
 
-// const copy = { name: "DUMMY", describe: accounting.describe };
+console.log(it);
 
-// shorthand
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Somethig went wrong..");
+accounting.printReports();
